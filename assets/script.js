bc5839c5d60495059c3601186caf3c2fdd52546b
Andy <84168944+AZurek17@users.noreply.github.com>
2023-10-08 16:31:31 -0700

    added weather Api Fetch

--- assets/script.js
+++ assets/script.js
@@ -1,83 +1,84 @@
 var apiKey = "16987757a5619c71a67c99c96ec1bd39";
 var btnSearch = document.querySelector("#btn-search");
 var cityInput = document.querySelector("#city-input");
+//--------------------
 var searchHistoryTable = document.querySelector("#search-history-table");
-var displayCityData = document.querySelector('ul');
+
+var displayCurrentForcast = document.querySelector('#display-current-forcast');
+
+//var displayFiveDayForcast = document.querySelector('ul');
 
 
 //------------------------
-var displayCityName = cityInput.value.trim();
-var cityLon = "";   // ********add info**********
-var cityLat = "";   // ********add info**********
+//var displayCityName = cityInput.value.trim();
 
 //------------------------
-
 function handleCitySearch(){
     console.log("clicked");
     var userInput = cityInput.value.trim();
 requestCoord(userInput);
+requestWeather();
 }
 
-// coorrdinates endpoint for city search
+// coords endpoint for city search
 function requestCoord(cityInput){
    var cityCoords = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput},&limit=1&appid=${apiKey}` 
    //var currentCityCoords;
-
-//----------------
+   //---------------------- 
    fetch(cityCoords)
    .then(function (response) {
-    console.log("fetchCords", response);
-    return response.json();
+        console.log("fetchCords", response);
+        return response.json();
    })
     .then(function (fetchedCoords){
-    console.log('Fetched Coords', fetchedCoords);//.name.lat.lon);
-    
-    //      console.log(fetchedCoords[i].name.lat.lon);
-    //  }
-    console.log("-------------");  // not needed, just for console.log display
-   })
-   
-}
+    console.log('Fetched Coords', fetchedCoords);
+    for (let i = 0; i < fetchedCoords.length; i++) {
+        console.log("-------------");  // log not needed, for console.log display
+        console.log("City Name", fetchedCoords[i].name);
+        console.log("City Coords lat", fetchedCoords[i].lat);/////
+        console.log("City Coords Lon", fetchedCoords[i].lon);/////
 
+
+        var cityName = document.createElement('h2');
+        cityName.textContent = (cityInput);
+        displayCurrentForcast.append(cityName);
+    };
+   })
+}
+//-------------------------------------------
 // weatherAPI endpoint 
-/// update lat and lon in var
-function requestWeather(){
-    var cityWeather =`https://api.openweathermap.org/data/2.5/forcast?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}&units=imperial`
+
+var latCity = fetchedCoords[i].lat;
+var lonCity = fetchedCoords[i].lon;
+
+
+function requestWeather(latCity, lonCity){
+    var cityWeather =`https://api.openweathermap.org/data/2.5/forcast?lat=${latCity}&lon=${lonCity}&appid=${apiKey}&units=imperial`
     //var weatherDisplayInfo;
-    
-//-----------------    
+
+    //-----------------    
     fetch(cityWeather)
-    .then(function (fetchWeather){
-    console.log("fecthWeather", fetchWeather);
-    return fetchWeather.json();
+    .then(function (response){
+    console.log("fecthWeather", response);
+    return response.json();
     })
     .then(function (fecthedWeather){
     console.log("Fetched City Weather", fecthedWeather);
-    cityWeather = fecthedWeather
+    cityWeather = fecthedWeather;
 
-    // for (let i = 0; i < fecthedWeather.length; i++) {
-    //     console.log(fecthedWeather[i].name.wind);//////////// update fecth endpoint items
-    //     var listCityInfo = document.createElement('li');
-    //     listData = document.createElement('p');
-    //     listData.textContent = fecthedWeather[i].name.wind; ///////update to grab city name
-    //     listCityInfo.appendChild(listData);
-    //     displayCityData.appendChild(listCityInfo);
-
-    })
+    
     console.log("-------------");
-    }
+    })
+}
 
 
-//------------------------------------------
 
-// function displayCityInfo(){
-//     var weaherDay =
-//     var cityName = 
-//     var cityTemp =
-//     var cityWind =
-//     var Humidity = 
+//--------added div classes and id's for
 
-// };
+var forcastCards = $("#forcast-card");
+var forcastDay = [1, 2, 3, 4, 5];
+
+
 
 
 btnSearch.addEventListener("click", handleCitySearch);
